c6aa66039e4cb11044f0656ae742d3a445ce734e
Sebbe <sebban952@gmail.com>
2026-04-03 09:18:11 +0200

    Refactor: renamed class, variables, and methods for clarity

--- am_space_invaders_demo-main/src/main/java/se/yrgo/game/Pillar.java
+++ am_space_invaders_demo-main/src/main/java/se/yrgo/game/Pillar.java
@@ -5,12 +5,12 @@ import java.awt.Rectangle;
 
 // this class can be much improved, better encapsulation
 // draw itself, update itself etc. etc.
-public class Alien {
+public class Pillar {
     public final int created;
     public final Rectangle topPillar;
     public final Rectangle bottomPillar;
 
-    public Alien(int created, int x, int gapY, int gapSize, int screenHeight) {
+    public Pillar(int created, int x, int gapY, int gapSize, int screenHeight) {
         this.created = created;
         this.topPillar = new Rectangle(x, 0, 60, gapY);
         this.bottomPillar = new Rectangle(x, gapY + gapSize, 60, screenHeight - gapY - gapSize);
